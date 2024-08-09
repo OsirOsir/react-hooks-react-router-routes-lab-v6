b9274181855f02ecebdeb272da1c3ba61d4c40bc
Philip osir Otieno <philiposir2@gmail.com>
2024-08-09 14:14:29 +0300

    updates

--- src/pages/Actors.js
+++ src/pages/Actors.js
@@ -1,12 +1,36 @@
 import { useEffect, useState } from "react";
+import NavBar from "../components/NavBar";
 
 function Actors() {
+
+  const [actors, setActors] = useState([])
+
+  useEffect(() => {
+    fetch("http://localhost:4000/actors")
+      .then(r => r.json())
+      .then(data => setActors(data))
+      .catch(error => console.error())
+  })
+
+
   return (
     <>
       <header>
         {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
+        <h1>Actors Page</h1>
+        {actors.map((actor) => (
+          <article key={actor.id}>
+            <h2>{actor.name}</h2>
+            <ul>
+              {actor.movies.map((movie, index) => (
+                <li key={index}>{movie}</li>
+              ))}
+            </ul>
+          </article>
+        ))}
         {/* Actor info here! */}
       </main>
     </>
--- src/pages/Movie.js
+++ src/pages/Movie.js
@@ -1,16 +1,39 @@
 import { useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+import NavBar from "../components/NavBar";
 
 function Movie() {
+  const [movieItem, setMovieItem] = useState({});
+  const { id } = useParams();  // Correctly destructure the id
+
+  useEffect(() => {
+    fetch(`http://localhost:4000/movies/${id}`)
+      .then((r) => r.json())
+      .then((data) => setMovieItem(data))
+      .catch((error) => console.error(error));
+  }, [id]);
+  
+
+  if (!movieItem.title) {
+    return <h1>Loading...</h1>;
+  }
+
   return (
     <>
       <header>
-        {/* What component should go here? */}
+        <NavBar />
       </header>
       <main>
-        {/* Movie info here! */}
+        <h1>{movieItem.title}</h1>
+        <p>Runtime: {movieItem.time} minutes</p>
+        <div>
+          {movieItem.genres.map((genre, index) => (
+            <span key={index}> {genre} </span>
+          ))}
+        </div>
       </main>
     </>
   );
-};
+}
 
 export default Movie;
